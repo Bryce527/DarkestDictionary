d2be9f148d395f7f391a36a9bf42f2079e60410a
nagatodht <dhtln0714@163.com>
2016-12-18 00:16:35 +0800

    Update ClientUI.java

--- src/Client/ClientUI.java
+++ src/Client/ClientUI.java
@@ -3,54 +3,84 @@ package Client;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.*;
-
+import java.util.Vector;
 
 public class ClientUI {
 	public static void main(String[] arg){
+		try {
+			org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper.launchBeautyEyeLNF();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
 		UI ui = new UI();
 		ui.init();
 		ui.Event();
 	}
 	
 	public static class UI{
-		//Ö÷´°¿Ú×é¼ş
+		//ä¸»çª—å£ç»„ä»¶
+		/*Cursor cs = new Cursor(Cursor.HAND_CURSOR);
+		private Icon ic=new ImageIcon("log_in.png");
+		private JLabel signIn = new JLabel(ic);*/
+		private Icon beforeLogin = new ImageIcon("beforelogin.png");
+		private Icon afterLogin = new ImageIcon("afterlogin.png");
+		private Icon beforeUp = new ImageIcon("beforedianzan.png");
+		private Icon afterUp = new ImageIcon("afterdianzan.png");
+		private Icon sendPicture = new ImageIcon("zhuanfa1.png");
 		
 		private JFrame frame;
-		Cursor cs=new Cursor(Cursor.HAND_CURSOR);
-		private Icon ic=new ImageIcon("G:\\Java_Files\\DarkestDictionary\\images\\log_in.png");
-//		private JButton signIn = new JButton("sign in");
-		private JButton signIn = new JButton(ic);
-		private JButton search = new JButton("search");
-		private JRadioButton rBaidu = new JRadioButton("Baidu");
-		private JRadioButton rBing = new JRadioButton("Bing");
-		private JRadioButton rYoudao = new JRadioButton("Youdao");
-		private JButton upBaidu = new JButton("Baidu");
-		private JButton upBing = new JButton("Bing");
-		private JButton upYoudao = new JButton("Youdao");
+		private JButton signIn = new JButton(beforeLogin);
+		private JButton search = new JButton(new ImageIcon("search_query_32px_1063411_easyicon.net.png"));;
+		private JRadioButton rBaidu = new JRadioButton("é‡‘å±±");
+		private JRadioButton rBing = new JRadioButton("å¿…åº”");
+		private JRadioButton rYoudao = new JRadioButton("æœ‰é“");
+		private JButton upBaidu = new JButton(beforeUp);
+		private JButton upBing = new JButton(beforeUp);
+		private JButton upYoudao = new JButton(beforeUp);
+		private JButton sendCard = new JButton(sendPicture);
+		//private JButton sendBing = new JButton(sendPicture);
+		//private JButton sendYoudao = new JButton(sendPicture);
 		private JTextArea result = new JTextArea();
 		private JTextField searchContent = new JTextField(75);
-		//µÇÂ½×Ó´°¿Ú×é¼ş
-
+		//ç‚¹èµ
+		private String wordTemp;
+		//ç™»é™†å­çª—å£ç»„ä»¶
+		boolean isExit = false; //isExitä¸ºtrue,ç”¨æˆ·ä¸ºç™»é™†çŠ¶æ€
+		private String userName;//è®°å½•æœ¬æœºç™»é™†ç”¨æˆ·å
+		
 		private JFrame signInFrame;
 		private JButton logIn = new JButton("log in");
 		private JButton signUp = new JButton("sign up");
 		private JLabel account = new JLabel("account");
 		private JLabel password = new JLabel("password");
 		private JTextField accountInput = new JTextField(30);
-		private JTextField passwordInput = new JTextField(30);
-		//Êı¾İ´«ËÍÏà¹Ø
+		private JTextField passwordInput = new JPasswordField(30);
+		//æ•°æ®ä¼ é€ç›¸å…³
 		private DataOutputStream toServer;
 		private DataInputStream fromServer;
 		private Socket client;
 		private int address;
 		
-		//³õÊ¼»¯
+		//å•è¯å¡ç›¸å…³
+		private Integer nPic = 0;//ç¦»çº¿æ—¶è·å¾—çš„å•è¯å¡æ•°ç›®
+		private JButton wordCard = new JButton(new ImageIcon("wordCard.png"));
+		private JLabel showNum = new JLabel();
+		
+		private JFrame sendWordFrame;
+		private JFrame wordCardFrame;
+		
+		private JButton nextWord = new JButton("next.png");
+		private JButton saveWord = new JButton("save.png");
+		
+		private Vector<BufferedImage> wordImagines;
+		//åˆå§‹åŒ–
 		public UI(){
 			frame = null;
 		};
-		
 		private void initNet() throws IOException{
 			address = (int)(1025 + Math.random()*65535);
 			client = new Socket("localhost",1025);
@@ -67,7 +97,7 @@ public class ClientUI {
 			//frame.add(new JButton("east"), BorderLayout.NORTH);
 			//frame.add(new JButton("east"), BorderLayout.CENTER);
 			
-			//µÇÂ½£¬ÊäÈë¿ò£¬ËÑË÷°´Å¥
+			//ç™»é™†ï¼Œè¾“å…¥æ¡†ï¼Œæœç´¢æŒ‰é’®
 			JPanel p1 = new JPanel();
 /*			p1.setLayout(new FlowLayout(FlowLayout.LEFT,5,5));
 			p1.add(signIn);
@@ -79,29 +109,48 @@ public class ClientUI {
 			p1.add(search, BorderLayout.EAST);
 			frame.add(p1, BorderLayout.NORTH);
 			
-			//Ñ¡ÔñÊ¹ÓÃµÄ·­ÒëÒÔ¼°µãÔŞ
+			//é€‰æ‹©ä½¿ç”¨çš„ç¿»è¯‘ä»¥åŠç‚¹èµ
 			JPanel p2 = new JPanel();
 			//p2.setLayout(new FlowLayout(FlowLayout.CENTER,5,5));
 			p2.setLayout(new BorderLayout(5,10));
 			//p2.setLayout(null);
 			JPanel p3 = new JPanel();
+			p3.setLayout(new BorderLayout(5,10));
 			JPanel p4= new JPanel();
-			p3.add(rBaidu);
+			JPanel p5 = new JPanel();
+			JPanel p6 = new JPanel();
+			JPanel p7 = new JPanel();
+			JPanel p8 = new JPanel();
+			p4.add(rBaidu);
 			//p3.add(new JRadioButton("Bing"));
-			p3.add(rBing);
+			p5.add(rBing);
 			//JPanel p4= new JPanel();
 			//p4.setLayout(new FlowLayout(FlowLayout.LEFT,5,5));
 			//p4.add(new JRadioButton("Youdao"));
-			p3.add(rYoudao);
+			p6.add(rYoudao);
 			//frame.add(p3,BorderLayout.CENTER);
 			//frame.add(p4,BorderLayout.WEST);
 			//JPanel p3 = new JPanel();
 			//p3.setLayout(new FlowLayout(FlowLayout.LEFT,5,5));
 			p4.add(upBaidu);
-			p4.add(upBing);
-			p4.add(upYoudao);
+			p5.add(upBing);
+			p6.add(upYoudao);
+			/*p4.add(sendBaidu);
+			p5.add(sendBing);
+			p6.add(sendYoudao);*/
+			p3.add(p4,BorderLayout.NORTH);
+			p3.add(p5,BorderLayout.CENTER);
+			p3.add(p6,BorderLayout.SOUTH);
+			
+			p8.setLayout(new BorderLayout(5,10));
+			p7.add(sendCard);
+			showNum.setText("å•è¯å¡æ•°é‡: " + nPic.toString());
+			p8.add(showNum, BorderLayout.SOUTH);
+			p8.add(wordCard,BorderLayout.CENTER);
+			
 			p2.add(p3,BorderLayout.NORTH);
-			p2.add(p4,BorderLayout.WEST);
+			p2.add(p7,BorderLayout.CENTER);
+			p2.add(p8,BorderLayout.SOUTH);
 			//p3.setPreferredSize(new Dimension(10,10));
 			//p2.setPreferredSize(new Dimension(10,10));
 			frame.add(p2,BorderLayout.WEST);
@@ -117,23 +166,11 @@ public class ClientUI {
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			frame.setVisible(true);
 			
-			try {
-				//initNet();
-				Socket socket = new Socket("localhost", 8000);
-				//Socket socket = new Socket("130.254.204.36", 8000);
-				//Socket socket = new Socket("drake.Armstrong.edu", 8000);
-				
-				//Create an input stream to receive data form the server
-				fromServer = new DataInputStream(socket.getInputStream());
-				
-				toServer = new DataOutputStream(socket.getOutputStream());
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			result.setLineWrap(true);
+			result.setWrapStyleWord(true);
 		};
 		
-		//ÏìÓ¦
+		//å“åº”
 		private class searchListener implements ActionListener{
 			public void actionPerformed(ActionEvent e) {
 				String word = searchContent.getText();
@@ -153,47 +190,75 @@ public class ClientUI {
 				
 				String strSend = "1" + "@" + rTemp + "@" + word;
 				
+				if(word.isEmpty()){
+					System.out.println("null");
+					wordTemp = "";
+				}
+				else{
+				
 				//result.setText(strSend);
-				try {
-					toServer.writeUTF(strSend);
-					toServer.flush();
-					String strOutput = fromServer.readUTF();
-					String[] outputArray = strOutput.split("@");
-					StringBuilder show = new StringBuilder();
-					for(int i = 0; i < outputArray.length; i ++) {
-						show.append(outputArray[i]);
-						show.append("\n\n");
+					wordTemp = word;
+					try {
+						toServer.writeUTF(strSend);
+						toServer.flush();
+						/*String strOutput = fromServer.readUTF();
+						String[] outputArray = strOutput.split("@");
+						StringBuilder show = new StringBuilder();
+						for(int i = 0; i < outputArray.length; i ++) {
+							show.append(outputArray[i]);
+							show.append("\n\n");
+						}*/
+						upBaidu.setIcon(beforeUp);
+						upBing.setIcon(beforeUp);
+						upYoudao.setIcon(beforeUp);
+						//result.setText(show.toString() + '\n');
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
 					}
-					result.setText(show.toString() + '\n');
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
 				}
 			}
 		}
 		
 		private class signInListener implements ActionListener{
 			public void actionPerformed(ActionEvent e){
-				signInFrame = new JFrame("sign in");
-				signInFrame.setLayout(new BorderLayout(5,10));
+				if(isExit == false){
+					signInFrame = new JFrame("sign in");
+					signInFrame.setLayout(new BorderLayout(5,10));
 				
-				JPanel p1 = new JPanel();
-				JPanel p2 = new JPanel();
-				JPanel p3 = new JPanel();
-				p1.add(account);
-				p1.add(accountInput);
-				p2.add(password);
-				p2.add(passwordInput);
-				p3.add(logIn);
-				p3.add(signUp);
-				signInFrame.add(p1, BorderLayout.NORTH);
-				signInFrame.add(p2, BorderLayout.CENTER);
-				signInFrame.add(p3, BorderLayout.SOUTH);
-				
-				signInFrame.setSize(500,300);
-				signInFrame.setLocationRelativeTo(null);
-				signInFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-				signInFrame.setVisible(true);
+					JPanel p1 = new JPanel();
+					JPanel p2 = new JPanel();
+					JPanel p3 = new JPanel();
+					p1.add(account);
+					p1.add(accountInput);
+					accountInput.setText("");
+					p2.add(password);
+					p2.add(passwordInput);
+					passwordInput.setText("");
+					p3.add(logIn);
+					p3.add(signUp);
+					signInFrame.add(p1, BorderLayout.NORTH);
+					signInFrame.add(p2, BorderLayout.CENTER);
+					signInFrame.add(p3, BorderLayout.SOUTH);
+					
+					signInFrame.setSize(500,300);
+					signInFrame.setLocationRelativeTo(null);
+					signInFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+					signInFrame.setVisible(true);
+				}
+				else{
+					String strSend = "2@" + userName;
+					try {
+						toServer.writeUTF(strSend);
+						toServer.flush();
+						//result.setText("Area received from the server is " + strOutput + '\n');
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					
+					signIn.setIcon(beforeLogin);
+				}
 			}
 		}
 		
@@ -202,35 +267,17 @@ public class ClientUI {
 				String strAccount = accountInput.getText();
 				String strPassword = passwordInput.getText();
 				String strSend = "0" + "@" + "0" + "@" + strAccount + "@" + strPassword;
-				String logInResult = "";
+				
 				try {
 					toServer.writeUTF(strSend);
 					toServer.flush();
-					logInResult = fromServer.readUTF();
+					
 					//result.setText("Area received from the server is " + strOutput + '\n');
 				} catch (IOException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 				//logInResult.compareTo("@suceed") == 0
-				if(logInResult.equals("2")){
-					signInFrame.dispatchEvent(new WindowEvent(signInFrame,WindowEvent.WINDOW_CLOSING));
-				}
-				else{
-/*					JFrame msgWindow = new JFrame();
-					
-					msgWindow.setSize(300,200);
-					msgWindow.setLocationRelativeTo(null);
-					msgWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-					msgWindow.setVisible(true);*/
-					if(logInResult.equals("1")) {		//ÃÜÂë´íÎó
-						JOptionPane.showMessageDialog(null, "ÃÜÂë´íÎó");
-					}
-					else if(logInResult.equals("0")) {	//ÓÃ»§Ãû²»´æÔÚ
-						JOptionPane.showMessageDialog(null, "ÓÃ»§Ãû²»´æÔÚ");
-					}
-						
-				}
 			}
 		}
 		
@@ -239,80 +286,105 @@ public class ClientUI {
 				String strAccount = accountInput.getText();
 				String strPassword = passwordInput.getText();
 				String strSend = "0" + "@" + "1" + "@" + strAccount + "@" + strPassword;
-				String signUpResult = "";
 				
 				try {
 					toServer.writeUTF(strSend);
 					toServer.flush();
-					signUpResult = fromServer.readUTF();
+					//signUpResult = fromServer.readUTF();
 					//result.setText("Area received from the server is " + strOutput + '\n');
 				} catch (IOException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
-				}
-				
-				if(signUpResult.equals("1")){	//×¢²á³É¹¦
-					signInFrame.dispatchEvent(new WindowEvent(signInFrame,WindowEvent.WINDOW_CLOSING));
-				}
-				else{		//×¢²áÊ§°Ü
-/*					JFrame msgWindow = new JFrame();
-					
-					msgWindow.setSize(300,200);
-					msgWindow.setLocationRelativeTo(null);
-					msgWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-					msgWindow.setVisible(true);*/
-					JOptionPane.showMessageDialog(null, "ÓÃ»§ÃûÒÑ´æÔÚ");
 				}
 			}
 		}
 		private class upBaiduListener implements ActionListener{
 			public void actionPerformed(ActionEvent e){
-				String strSend = "@3@Baidu";
-				String logInResult = "";
+				if(isExit == true&&wordTemp.isEmpty() == false){
+					String strSend = "3@jinshan@" + wordTemp;
+					String logInResult = "";
 				
-/*				try {
-					toServer.writeUTF(strSend);
-					toServer.flush();
-					logInResult = fromServer.readUTF();
-					//result.setText("Area received from the server is " + strOutput + '\n');
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}*/
+					upBaidu.setIcon(afterUp);
+					try {
+						toServer.writeUTF(strSend);
+						toServer.flush();
+						//logInResult = fromServer.readUTF();
+						//result.setText("Area received from the server is " + strOutput + '\n');
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
 			}
 		}
 		private class upBingListener implements ActionListener{
 			public void actionPerformed(ActionEvent e){
-				String strSend = "@3@Bing";
-				String logInResult = "";
+				if(isExit == true&&wordTemp.isEmpty() == false){
+					String strSend = "3@bing@" + wordTemp;
+					String logInResult = "";
 				
-/*				try {
-					toServer.writeUTF(strSend);
-					toServer.flush();
-					logInResult = fromServer.readUTF();
-					//result.setText("Area received from the server is " + strOutput + '\n');
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}*/
+					upBing.setIcon(afterUp);
+					try {
+						toServer.writeUTF(strSend);
+						toServer.flush();
+						//logInResult = fromServer.readUTF();
+						//result.setText("Area received from the server is " + strOutput + '\n');
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
 			}
 		}
 		private class upYoudaoListener implements ActionListener{
 			public void actionPerformed(ActionEvent e){
-				String strSend = "@3@Youdao";
-				String logInResult = "";
+				if(isExit == true&&wordTemp.isEmpty() == false){
+					String strSend = "3@youdao@" + wordTemp;
+					String logInResult = "";
 				
-/*				try {
-					toServer.writeUTF(strSend);
-					toServer.flush();
-					logInResult = fromServer.readUTF();
-					//result.setText("Area received from the server is " + strOutput + '\n');
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}*/
+					upYoudao.setIcon(afterUp);
+					try {
+						toServer.writeUTF(strSend);
+						toServer.flush();
+						//logInResult = fromServer.readUTF();
+						//result.setText("Area received from the server is " + strOutput + '\n');
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
 			}
 		}
+		
+		private class sendWordListener implements ActionListener{
+			public void actionPerformed(ActionEvent e){
+				if(wordTemp.isEmpty());
+				else{
+					sendWordFrame = new JFrame("sendWord");
+					sendWordFrame.setLayout(new BorderLayout(5,10));
+					
+					sendWordFrame.setLocationRelativeTo(null);
+					sendWordFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+					sendWordFrame.setVisible(true);
+				}
+			}
+		}
+		
+		private class wordCardListener implements ActionListener{
+			public void actionPerformed(ActionEvent e){
+				wordCardFrame = new JFrame("wordCard");
+				wordCardFrame.setLayout(new BorderLayout(5,10));
+				
+				JPanel p1 = new JPanel();
+				JPanel p2 = new JPanel();
+				
+				p2.setLayout(new BorderLayout(5,10));
+				p2.add(nextWord, BorderLayout.EAST);
+				p2.add(saveWord, BorderLayout.WEST);
+			}
+		}
+		
+		
 		private void Event(){
 			signIn.addActionListener(new signInListener());
 			search.addActionListener(new searchListener());
@@ -321,6 +393,93 @@ public class ClientUI {
 			upBaidu.addActionListener(new upBaiduListener());
 			upBing.addActionListener(new upBingListener());
 			upYoudao.addActionListener(new upYoudaoListener());
+			
+			try {
+				//initNet();
+				Socket socket = new Socket("223.104.147.167", 8000);
+				//Socket socket = new Socket("130.254.204.36", 8000);
+				//Socket socket = new Socket("drake.Armstrong.edu", 8000);
+				
+				//Create an input stream to receive data form the server
+				fromServer = new DataInputStream(socket.getInputStream());
+				
+				toServer = new DataOutputStream(socket.getOutputStream());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			while(true){
+				try {
+					String strOutput = fromServer.readUTF();
+					String[] outputArray = strOutput.split("@");
+					System.out.println(strOutput);
+					
+					
+					if(outputArray[0].equalsIgnoreCase("1")){//search
+						StringBuilder show = new StringBuilder();
+						for(int i = 1; i < outputArray.length;) {
+							show.append(outputArray[i]);
+							show.append("\n");
+							i++;
+							show.append(outputArray[i]);
+							show.append("\n");
+							i++;
+							show.append("ç‚¹èµæ¬¡æ•°: " + outputArray[i]);
+							i++;
+							show.append("\n\n");
+						}
+						result.setText(show.toString() + '\n');
+					}
+					else if(outputArray[0].equalsIgnoreCase("0")){
+						if(outputArray[1].equalsIgnoreCase("0")){//æ³¨å†Œ
+							if(outputArray[2].equalsIgnoreCase("1")){
+								signInFrame.dispatchEvent(new WindowEvent(signInFrame,WindowEvent.WINDOW_CLOSING));
+								
+								isExit = true;
+								userName = outputArray[4];
+								signIn.setIcon(afterLogin);
+							}
+							else{
+								JOptionPane.showMessageDialog(null, "ç”¨æˆ·åå·²å­˜åœ¨");
+							}
+						}
+						else if(outputArray[1].equalsIgnoreCase("1")){//ç™»é™†
+							if(outputArray[2].equalsIgnoreCase("2")){
+								signInFrame.dispatchEvent(new WindowEvent(signInFrame,WindowEvent.WINDOW_CLOSING));
+								
+								isExit = true;
+								userName = outputArray[3];
+								signIn.setIcon(afterLogin);
+							}
+							else if(outputArray[2].equalsIgnoreCase("1")){
+								JOptionPane.showMessageDialog(null, "å¯†ç é”™è¯¯");
+							}
+							else if(outputArray[2].equalsIgnoreCase("0")){
+								JOptionPane.showMessageDialog(null, "ç”¨æˆ·åä¸å­˜åœ¨");
+							}
+						}
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 		}
 	}
+}
+
+class ImagePanel extends JPanel {  
+	  
+    private BufferedImage image;  
+  
+    public ImagePanel(BufferedImage tempImagine) {   
+            image = tempImagine;   
+    }  
+  
+    @Override  
+    public void paintComponent(Graphics g) {  
+        g.drawImage(image, 0, 0, null);   
+    }  
+  
 }
